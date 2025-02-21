eda5ad4219e94ed86799944cd4fc69690798791b
tejaswi-28 <tejaswilatika28@gmail.com>
2025-02-21 13:19:41 +0530

    updated routing

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ExpressInterestComponent } from '../express-interest/express-interest.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
+  @ViewChild(ExpressInterestComponent) formPopup!: ExpressInterestComponent;
 
+  openFormPopup() {
+    this.formPopup.openPopup();
+  }
 }
