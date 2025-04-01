177e2d0227213f73d8e69ebb59e026a4681b0a87
tejaswi-28 <tejaswilatika28@gmail.com>
2025-04-01 14:07:13 +0530

    updated past session card and font style

--- src/app/components/past-sessions/past-sessions.component.ts
+++ src/app/components/past-sessions/past-sessions.component.ts
@@ -5,23 +5,32 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-past-sessions',
   templateUrl: './past-sessions.component.html',
-  styleUrl: './past-sessions.component.css'
+  styleUrl: './past-sessions.component.css',
 })
-export class PastSessionsComponent implements OnInit{
+export class PastSessionsComponent implements OnInit {
   classes: any[] = [];
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    this.authService.data$.subscribe(
-      (data) => {
-        this.classes = data;
-      }
-    )
+    this.authService.data$.subscribe((data) => {
+      this.classes = data;
+    });
   }
 
   goToCourses(classItem: any): void {
     const formattedClassName = classItem.className.replace(/ /g, '-');
     this.router.navigate(['/class-detail', formattedClassName]);
+  }
+
+  convertLocalDateToReadableFormat(dateStr: string): string {
+    if (!dateStr) return '';
+    const [year, month, day] = dateStr.split('-').map(Number);
+    const date = new Date(year, month - 1, day);
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    }).format(date);
   }
 }
